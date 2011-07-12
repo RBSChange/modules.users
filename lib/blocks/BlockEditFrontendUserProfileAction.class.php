81d1b17d54fb6be22f8d552e2d47f29a6cce07f3
Gaël PORT <gael.port@rbs.fr>
2011-07-12 17:03:25 +0200

    [FIX] Title save.

--- lib/blocks/BlockEditFrontendUserProfileAction.class.php
+++ lib/blocks/BlockEditFrontendUserProfileAction.class.php
@@ -11,25 +11,36 @@ class users_BlockEditFrontendUserProfileAction extends website_BlockAction
 	 * @param f_mvc_Response $response
 	 * @return String
 	 */
-	function execute($request, $response)
-    {
+	public function execute($request, $response)
+	{
 		if ($this->isInBackoffice())
 		{
 			return website_BlockView::INPUT;
 		}
-		
+
 		$user = users_UserService::getInstance()->getCurrentFrontEndUser();
 		$request->setAttribute('user', $user);
-		
+
 		return website_BlockView::INPUT;
-    }
-    
-    public function saveNeedTransaction()
-    {
-    	return true;
-    }
-    
-    /**
+	}
+
+	/**
+	 * @return boolean
+	 */
+	public function saveNeedTransaction()
+	{
+		return true;
+	}
+	
+	/**
+	 * @return string[]
+	 */
+	public function getUserBeanExclude()
+	{
+		return array('title');
+	}
+
+	/**
 	 * @param f_mvc_Request $request
 	 * @param f_mvc_Response $response
 	 * @param users_persistentdocument_websitefrontenduser $user
@@ -42,27 +53,25 @@ class users_BlockEditFrontendUserProfileAction extends website_BlockAction
 			$user->setLogin($user->getEmail());
 		}
 		$user->save();
-		
+
 		//TODO: Email confirmation.
 		//$user->getDocumentService()->sendEmailConfirmationMessage($user, false);
-		
-		$this->addMessage(f_Locale::translate('&modules.users.frontoffice.Informations-updated;'));
-		
+
+		$this->addMessage(LocaleService::getInstance()->transFO('m.users.frontoffice.informations-updated', array('ucf', 'html')));
+
 		return website_BlockView::INPUT;
 	}
-	
+
 	/**
 	 * @param f_mvc_Request $request
 	 * @param users_persistentdocument_websitefrontenduser $user
 	 */
 	public function validateSaveInput($request, $user)
 	{
-		$includedFields = array('email');
-		$includedFields[] = 'firstname';
-		$includedFields[] = 'lastname';
+		$includedFields = array('email', 'titleid', 'firstname', 'lastname');
 		$validationRules = BeanUtils::getBeanValidationRules('users_persistentdocument_websitefrontenduser', $includedFields);
 		$isOk = $this->processValidationRules($validationRules, $request, $user);
-		
+
 		// Login validation.
 		if ($user->isPropertyModified('login'))
 		{
@@ -70,12 +79,12 @@ class users_BlockEditFrontendUserProfileAction extends website_BlockAction
 			$login = ($request->hasParameter('login')) ? $request->getParameter('login') : $request->getParameter('email');
 			if (in_array($login, users_ModuleService::getInstance()->getDisallowedLogins()))
 			{
-				$this->addError(f_Locale::translate('&modules.users.frontoffice.Login-disallowed;'));
+				$this->addError(LocaleService::getInstance()->transFO('m.users.frontoffice.login-disallowed', array('ucf', 'html')));
 				$isOk = false;
 			}
 			else if (users_UserService::getInstance()->getFrontendUserByLogin($login, $website->getId()))
 			{
-				$this->addError(f_Locale::translate('&modules.users.frontoffice.Login-used;'));
+				$this->addError(LocaleService::getInstance()->transFO('m.users.frontoffice.login-used', array('ucf', 'html')));
 				$isOk = false;
 			}
 		}
