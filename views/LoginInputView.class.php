05c8fe4e2ab3913efed3ef820bb8a7f93ae0e1d5
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-08-01 16:40:57 +0200

    [NEW #187] Atteindre et ouvrir un editeur bo avec une url

--- views/LoginInputView.class.php
+++ views/LoginInputView.class.php
@@ -6,30 +6,34 @@ class users_LoginInputView extends f_view_BaseView
 	 * @param Request $request
 	 */
 	public function _execute($context, $request)
-    {
-    	$rq = RequestContext::getInstance();
-		try 
+	{
+		$rq = RequestContext::getInstance();
+		try
 		{
 			$lang = $rq->getUILang();
-        	$rq->beginI18nWork($lang);
-        	$this->forceModuleName("users");
-        	$templateName = 'Users-Login-'.$lang;
-        	$path = TemplateResolver::getInstance()->setPackageName('modules_users')->setDirectory('templates')->getPath($templateName);        	
-        	if ($path === null)
-        	{
-        		$templateName = 'Users-Login-en';
-        	}
+			$rq->beginI18nWork($lang);
+			$this->forceModuleName("users");
+			$templateName = 'Users-Login-' . $lang;
+			$path = TemplateResolver::getInstance()->setPackageName('modules_users')->setDirectory('templates')->getPath($templateName);
+			if ($path === null)
+			{
+				$templateName = 'Users-Login-en';
+			}
 			$this->setTemplateName($templateName, K::HTML);
 			if (!$request->hasParameter('access'))
 			{
 				Framework::warn(__METHOD__ . " No informations to display an authentication in front office");
 			}
-	        $this->setAttribute('UIHOST',  Framework::getUIBaseUrl());
+			$this->setAttribute('UIHOST', Framework::getUIBaseUrl());
+			$link = new f_web_ChromeParametrizedLink('rbschange/content/ext/' . PROJECT_ID);
+			$link->setQueryParameters(array('module' => 'uixul', 'action' => 'Admin'));
+			$link->setArgSeparator(f_web_HttpLink::ESCAPE_SEPARATOR);
+			$this->setAttribute('xchromeURL', $link->getUrl());
 			$rq->endI18nWork();
 		}
 		catch (Exception $e)
 		{
 			$rq->endI18nWork($e);
 		}
-	 }
+	}
 }
