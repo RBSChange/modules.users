7d445341718ea62f64686b27f22b84a8c89a1dcb
Eric HAUSWALD <eric.hauswald@rbs.fr>
2012-09-13 14:55:35 +0200

    [UPDATE] Use standard injection

--- lib/ChangeController.class.php
+++ lib/ChangeController.class.php
@@ -8,38 +8,41 @@ class users_ChangeController extends change_Controller
 	 */
 	public function dispatch()
 	{
-		// Handle auto-login.
-		$us = users_UserService::getInstance();
-		if (isset($_COOKIE[self::AUTO_LOGIN_COOKIE]) && $us->getCurrentUser() === null)
+		if (!($this instanceof change_XulController))
 		{
-			$autoLoginInfos = $_COOKIE[self::AUTO_LOGIN_COOKIE];
-			$login = $autoLoginInfos['login'];
-			$passwd = $autoLoginInfos['passwd'];
-			
-			$website = website_WebsiteService::getInstance()->getCurrentWebsite();
-			$users = $us->getUsersByLoginAndGroup($login, $website->getGroup());
-			$ok = false;
-			foreach ($users as $user) 
+			// Handle auto-login.
+			$us = users_UserService::getInstance();
+			if (isset($_COOKIE[self::AUTO_LOGIN_COOKIE]) && $us->getCurrentUser() === null)
 			{
-				if (sha1($user->getPasswordmd5()) == $passwd)
+				$autoLoginInfos = $_COOKIE[self::AUTO_LOGIN_COOKIE];
+				$login = $autoLoginInfos['login'];
+				$passwd = $autoLoginInfos['passwd'];
+				
+				$website = website_WebsiteService::getInstance()->getCurrentWebsite();
+				$users = $us->getUsersByLoginAndGroup($login, $website->getGroup());
+				$ok = false;
+				foreach ($users as $user) 
+				{
+					if (sha1($user->getPasswordmd5()) == $passwd)
+					{
+						if (Framework::isInfoEnabled())
+						{
+							Framework::info(__METHOD__ . ' auto-login with ' . $login);
+						}
+						$us->authenticate($user);
+						$ok = true;
+						break;
+					}
+				}
+				
+				if (!$ok)
 				{
 					if (Framework::isInfoEnabled())
 					{
-						Framework::info(__METHOD__ . ' auto-login with ' . $login);
+						Framework::info(__METHOD__ . ' auto-login skipped because the user with login ' . $login . ' doesn\'t exist, is not published or the password is wrong.');
 					}
-					$us->authenticate($user);
-					$ok = true;
-					break;
+					users_ModuleService::getInstance()->unsetAutoLogin();
 				}
-			}
-			
-			if (!$ok)
-			{
-				if (Framework::isInfoEnabled())
-				{
-					Framework::info(__METHOD__ . ' auto-login skipped because the user with login ' . $login . ' doesn\'t exist, is not published or the password is wrong.');
-				}
-				users_ModuleService::getInstance()->unsetAutoLogin();
 			}
 		}
 		parent::dispatch();
