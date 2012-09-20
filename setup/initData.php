fc2e319ff0b9827e3247c076b1c23b620047e77c
Gaël PORT <gael.port@rbs.fr>
2012-09-20 09:17:25 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -3,27 +3,17 @@ class users_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-			$scriptReader->executeModuleScript('users', 'init.xml');
-			$scriptReader->executeModuleScript('users', 'resetpasswordnotif.xml');
-			$scriptReader->executeModuleScript('users', 'lists.xml');
-			
-			$this->getTransactionManager()->beginTransaction();
+		$scriptReader = import_ScriptReader::getInstance();
+		$this->executeModuleScript('init.xml');
+		
+		$this->getTransactionManager()->beginTransaction();
 
-			$wwwadmins = users_UserService::getInstance()->getUsersByLoginAndGroup('wwwadmin', users_BackendgroupService::getInstance()->getBackendGroup());
-			$wwwadmin = $wwwadmins[0];
-			$wwwadmin->setPasswordmd5(null);
-			$wwwadmin->setEmail(null);
-			$this->getPersistentProvider()->updateDocument($wwwadmin);
-			
-			$this->getTransactionManager()->commit();
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
+		$wwwadmins = users_UserService::getInstance()->getUsersByLoginAndGroup('wwwadmin', users_BackendgroupService::getInstance()->getBackendGroup());
+		$wwwadmin = $wwwadmins[0];
+		$wwwadmin->setPasswordmd5(null);
+		$wwwadmin->setEmail(null);
+		$this->getPersistentProvider()->updateDocument($wwwadmin);
+		
+		$this->getTransactionManager()->commit();
 	}
 }
